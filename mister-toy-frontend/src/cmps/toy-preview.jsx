125f02cabd7e4e952bb1286958db34317c748b99
Amir Yakubov <yakobub@gmail.com>
2023-01-13 02:40:35 +0200

    add imgs

--- mister-toy-frontend/src/cmps/toy-preview.jsx
+++ mister-toy-frontend/src/cmps/toy-preview.jsx
@@ -1,12 +1,15 @@
 
-export function ToyPreview({ toy }) {
-    return (
 
+export function ToyPreview({ toy }) {
+
+
+    const imgUrl = toy.imgUrl
+
+    return (
         <article>
             <h4>{toy.name}</h4>
-            <h1></h1>
+            <img src={require(`../assets/img/${imgUrl}`)} />
             <p>Price: <span>${toy.price}</span></p>
-
         </article>
     )
 }
